27818fa806c44f8a6fae9a215fd9c64ee800b065
Miles <lynnprosper@gmail.com>
2015-05-19 22:25:59 +0800

    chapter2-3
    成员资格检查：x in y.

--- 2.py
+++ 2.py
@@ -1,14 +1,8 @@
-sentence = raw_input("sentence: ")
+database = [
+	['albert', '1234']
+]
 
-screen_width = 80
-text_width = len(sentence)
-box_width = text_width + 6
-left_margin = (screen_width - box_width) / 2
-
-print 
-print ' ' * left_margin + '+' + '-' * (box_width - 4) + '+'
-print ' ' * left_margin + '|' + ' ' * text_width + '|'
-print ' ' * left_margin + '|' + sentence + '|'
-print ' ' * left_margin + '|' + ' ' * text_width + '|'
-print ' ' * left_margin + '+' + '-' * (box_width - 2) + '+'
-print
+username = raw_input('user  name: ')
+pin = raw_input('pin code: ')
+if [username, pin] in database:
+	print 'access granted'
