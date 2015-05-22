7f63ee9af280cfaa643c4e256c940f1849addd95
Miles <lynnprosper@gmail.com>
2015-05-22 17:40:28 +0800

    chapter2-4
    字符串格式化

--- 2.py
+++ 2.py
@@ -1,8 +1,18 @@
-database = [
-	['albert', '1234']
-]
+width = input('Please enter width: ')
 
-username = raw_input('user  name: ')
-pin = raw_input('pin code: ')
-if [username, pin] in database:
-	print 'access granted'
+price_width = 10
+item_width = width - price_width
+header_format = '%-*s%*s'
+format = '%-*s%*.2f'
+
+print '=' * width
+
+print header_format % (item_width, 'item', price_width, 'price')
+print '-' * width
+print format % (item_width, 'Apples', price_width, 0.4)
+print format % (item_width, 'pears', price_width, 0.5)
+print format % (item_width, 'cantaloupes', price_width, 1.92)
+print format % (item_width, 'dried apricots (16 oz.)', price_width, 8)
+print format % (item_width, 'Prunes (4 lbs.)', price_width, 12)
+
+print '=' * width
